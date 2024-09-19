f4634aebbbe06b4abfe8a028469271ee35645fd8
Joseloper <mylord331@gmail.com>
2024-09-18 20:23:15 -0500

    Adding comments

--- main.rb
+++ main.rb
@@ -1,12 +1,19 @@
+#Create a cypher that uses ascii numbers to encode or decode the message, this encoder allows blank spaces, special characters and caps
+
+
+
+#encode method 
 def encode(phrase, shifter)
   traslated = phrase.chars.map  {|c|c.ord + shifter}
   final = traslated.map {|c|c.chr}.join
   return final
 end
-
+ #decode method
 def decode(phrase, shifter)
   traslated = phrase.chars.map  {|c|c.ord - shifter}
   final = traslated.map {|c|c.chr}.join
   return final
 end
+
+
 
