5b0d3fef705d5ee37c3e00e360331760812042e9
Joseloper <mylord331@gmail.com>
2024-10-01 20:17:39 -0500

    adding bundler and rubocop gem to check the errors in the caesar cypher logic

--- caesar_cypher/encoder.rb
+++ caesar_cypher/encoder.rb
@@ -1,19 +1,13 @@
-#Create a cypher that uses ascii numbers to encode or decode the message, this encoder allows blank spaces, special characters and caps
+# Create a cypher that uses ascii numbers to encode or decode the message, this encoder allows blank spaces, special characters and caps
 
-
-
-#encode method 
+# encode method
 def encode(phrase, shifter)
-  traslated = phrase.chars.map  {|c|c.ord + shifter}
-  final = traslated.map {|c|c.chr}.join
-  return final
+  traslated = phrase.chars.map { |c| c.ord + shifter }
+  traslated.map { |c| c.chr }.join
 end
- #decode method
+
+# decode method
 def decode(phrase, shifter)
-  traslated = phrase.chars.map  {|c|c.ord - shifter}
-  final = traslated.map {|c|c.chr}.join
-  return final
+  traslated = phrase.chars.map { |c| c.ord - shifter }
+  traslated.map { |c| c.chr }.join
 end
-
-
-
